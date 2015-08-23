10abf4946d3fe1bed4d39027ee30eb3aa33ad592
liv123 <olivia.e@gmail.com>
2015-08-23 16:06:54 -0700

    added player func to app.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+
 // OOP Racing Game example boilerplate code
 
 function Game() {
   //Create a new instance of player 1
   //this.player1 = ...
+  player1 = new Player ('red', 'x', 97)
+
 
   //Do the same for a player 2
   //this.player2 = ...
+  player2 = new Player ('blue', 'i', 108)
 
   //Create the track
   //this.track = ...
+
 }
 
 // `Game.prototype.init` kicks off a new game with a board and two players
@@ -17,9 +22,10 @@ Game.prototype.init = function() {
 };
 
 // A starter Player constructor.
-function Player(team) {
-  //this.name = ...
-  //this.position = ...
+function Player(color, letterUsed) {
+  this.color = color;
+  this.position = 0;
+  this.letterUsed = letterUsed;
 };
 
 // Remember: prototypes are shared functions between all game instances
